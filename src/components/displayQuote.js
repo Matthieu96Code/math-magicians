024a301b4ecef3d06101cd29f6b3ae01055ce0ac
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-03-23 15:08:16 +0000

    fetch the data

--- src/components/displayQuote.js
+++ src/components/displayQuote.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 
-const Quotes = () => (
-  <div>
-    quote
-  </div>
-);
+const apiUrl = 'https://api.api-ninjas.com/v1/quotes?category=happiness';
+const key = 'BQMsoYBppXMsrv0ST47LtA==ZtlLHHrcDDqIdkSA';
+
+const Quotes = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(apiUrl,
+        {
+          headers: {
+            'X-Api-Key': key,
+          },
+        });
+      const json = await res.json();
+      setData(json[0]);
+    };
+    fetchData();
+  }, [setData]);
+  console.log(data);
+
+  return (
+    <div>
+      qoute
+    </div>
+  );
+};
 
 export default Quotes;
