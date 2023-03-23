351a5613e8b244f90aba4d755099746aa97eb9fe
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-03-23 16:44:54 +0000

    import quote component to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Calculator from './components/Calculator';
+import Quotes from './components/displayQuote';
 
 function App() {
   return (
     <div className="App">
       <Calculator />
+      <Quotes />
     </div>
   );
 }
