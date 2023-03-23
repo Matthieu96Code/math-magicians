f8fb7069679811e67b9e342e1a1fb5f9f1d1b199
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-03-23 16:41:22 +0000

    show loading et error state

--- src/components/displayQuote.js
+++ src/components/displayQuote.js
@@ -4,26 +4,29 @@ const apiUrl = 'https://api.api-ninjas.com/v1/quotes?category=happiness';
 const key = 'BQMsoYBppXMsrv0ST47LtA==ZtlLHHrcDDqIdkSA';
 
 const Quotes = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
-      const res = await fetch(apiUrl,
-        {
-          headers: {
-            'X-Api-Key': key,
-          },
-        });
-      const json = await res.json();
-      setData(json[0]);
+      try {
+        const res = await fetch(apiUrl,
+          {
+            headers: {
+              'X-Api-Key': key,
+            },
+          });
+        const json = await res.json();
+        setData(json[0].quote);
+      } catch (err) {
+        setData(err.message);
+      }
     };
     fetchData();
   }, [setData]);
-  console.log(data);
 
   return (
     <div>
-      qoute
+      {data || 'loading ...'}
     </div>
   );
 };
