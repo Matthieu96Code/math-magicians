8da2b5a0483a074a7b59bc0c8146d94dc0cada06
Kodjo Matthieu SENOU <senoumatthieu96@gmail.com>
2023-03-24 07:11:28 +0000

    Revert "Fetch data from api"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Calculator from './components/Calculator';
-import Quotes from './components/displayQuote';
 
 function App() {
   return (
     <div className="App">
       <Calculator />
-      <Quotes />
     </div>
   );
 }
