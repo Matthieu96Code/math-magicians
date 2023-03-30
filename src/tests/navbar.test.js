cac726fc5458a3658f3ad6244512cd6387754e8b
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-03-30 06:44:39 +0000

    link test in navbar

--- src/tests/navbar.test.js
+++ src/tests/navbar.test.js
@@ -13,4 +13,16 @@ describe('Navbar component', () => {
     expect(container.firstChild).toMatchSnapshot();
   });
 
+  it('contains three navigation links', () => {
+    const { getAllByRole } = render(
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>,
+    );
+    const links = getAllByRole('link');
+    expect(links.length).toBe(3);
+    expect(links[0]).toHaveTextContent('Home');
+    expect(links[1]).toHaveTextContent('Calculator');
+    expect(links[2]).toHaveTextContent('Quote');
+  });
 });
